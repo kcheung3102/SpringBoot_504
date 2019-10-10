ca802eca1de329d25eb0d92915a8dd1935e709e3
Kevin Cheung <44646134+kcheung3102@users.noreply.github.com>
2019-10-09 23:45:09 -0400

    password fix

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,18 +1,24 @@
 package com.example.demo;
 
 import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends
     WebSecurityConfigurerAdapter{
-
+    @Bean
+    public  PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +37,7 @@ public class SecurityConfiguration extends
         protected void configure(AuthenticationManagerBuilder auth)
         throws Exception{
             auth.inMemoryAuthentication().withUser("user")
-                    .password("password").authorities("USER");
+                    .password(passwordEncoder().encode("password")).authorities("USER");
 
         }
     }
